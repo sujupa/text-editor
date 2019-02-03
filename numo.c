f1fb2be8eb25aa35728c2543bdd8f378bcce81d0
Sujay Patil <33329820+sujupa@users.noreply.github.com>
2019-02-03 15:23:08 +0530

    Update numo.c

--- numo.c
+++ numo.c
@@ -3,6 +3,6 @@
 int main()
 {
   char c;
-  while(read(STDIN_FILENO, &c, 1) == 1 && c!='q'); //1 here is one byte..we are telling it to read 1 byte until 'q' is typed
+  while(read(STDIN_FILENO, &c, 1) == 1 && c!='q'); //1 here is one byte..we are telling it to read 1 byte in loop until 'q' is typed
   return 0;
 }
